ab6f8fc41e0a5897f3c0a6f03747bed69e440795
Roberto Maciel <lmaciel1303@gmail.com>
2020-08-26 16:29:17 -0500

    Use slug instead id for our routes

--- app/Post.php
+++ app/Post.php
@@ -8,4 +8,11 @@ class Post extends Model
 {
     // Set mass-assignable fields
     protected $fillable = ['title', 'content', 'category', 'slug'];
+
+    /* Change the key for our model to use Route Model Binding 
+    instead of using the id we're using the slug field, it needs to be unique*/
+    public function getRouteKeyName()
+	{
+	    return 'slug';
+	}
 }
