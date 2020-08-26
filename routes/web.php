8640599ac172f88defbea75f1aaa32325bc09cfd
Roberto Maciel <lmaciel1303@gmail.com>
2020-08-26 14:22:51 -0500

    Add routes for Posts
    Routes for Posts added in routes/web.api.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* If you want to overwrite a route of the resource, you need to put it before the route resource
+in this case we're taking the route /posts that is related to index method to / which is now also related to index method */
+Route::get('/', 'PostController@index');
+/* With except you can define which routes you don't want to generate, in this case we don't need a /posts route for index method because we defined / for this method.
+You can list your routes with and without the except to see the difference */
+Route::resource('posts', 'PostController')->except([
+    'index',
+]);
