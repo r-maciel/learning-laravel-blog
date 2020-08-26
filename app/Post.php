b38e8ce3ff52d69974c6de6e7c17a3130d054ded
Roberto Maciel <lmaciel1303@gmail.com>
2020-08-26 15:51:39 -0500

    Enable mass assignment Post Model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Set mass-assignable fields
+    protected $fillable = ['title', 'content', 'category', 'slug'];
 }
